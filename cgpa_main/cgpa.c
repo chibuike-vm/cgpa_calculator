2b9b8ef755bf310287d6b13bbd70158f96a1dff3
Chibuike Mba <chibuike.code@gmail.com>
2023-02-24 22:22:12 +0100

    Add Changes in cgpa.c to Reflect Changes in cgpa.h

--- cgpa_main/cgpa.c
+++ cgpa_main/cgpa.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../cgpa_header/cgpa.h"
+
 /**
  * main - Program Entry Point.
  *
@@ -13,33 +16,30 @@
  */
 int main(void)
 {
-	printf("\n\n------UNIVERISTY CGPA CALCULATOR (GLOBAL STANDARD)-------\n");
+	/* Obeying the ISO C89/90 Standard */
 	int nYrs;
 
+	printf("\n\n------UNIVERISTY CGPA CALCULATOR (GLOBAL STANDARD)-------\n");
 	putchar('\n');
-	printf(" Please, (in figures only) kindly enter the duration\n");
-	printf(" of your university degree programme: ");
-	scanf("%d", &nYrs);
-	putchar('\n');
+
+	nYrs = nYrsInputValidator();
 
 	if (nYrs >= 1 && nYrs <= 6)
 	{
 		/* float (*cgpaFptr)(int nYrs) = cgpa; */
-		float (*cgpaFptr)(int nYrs);
+		float (*cgpaFptr)(int nYr);
 
 		/**
-		 * The use of "&" operator in function pointer is not
-		 * neccessary as they are implicitly present/used just
-		 * like in arrays where "&" operator are also implictly
-		 * used.
+		 * The use of "&" operator in function pointers is not
+		 * neccessary as they are implicitly present/used just like in
+		 * arrays where "&" operator are also implictly used.
 		 */
 		cgpaFptr = cgpa;
-
 		cgpaGraduation(nYrs, cgpaFptr);
 	}
 	else
 	{
-		printf(" Please, try again and enter a valid year of study\n between ");
+		printf(" \n Please, try again and enter a valid year of study\n between ");
 		printf(" 1 - 6.\n\n Thank you.\n\n");
 		printf("\n-------CREATED BY @chibuike-vm (GITHUB USERNAME)--------\n");
 	}
